5f91dd6601cf5f0aaf01f6980aa8a3917111edd3
K_Vova <karmazynvolodymyr>
2020-03-22 09:27:25 +0200

    Fix Annotation

--- home-work-21/src/volodymyr/Application.java
+++ home-work-21/src/volodymyr/Application.java
@@ -1,28 +1,53 @@
 package volodymyr;
 
-import java.lang.reflect.Field;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.lang.reflect.AnnotatedElement;
+import java.text.Format.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
-	public static void main(String[] args) {
-		
-		getMyCustomAnnotation(Person.class);
-		
+	public static void main(String[] args) throws Exception {
+
+		List<String> myCustomAnnotationList = getAnnotation(Person.class);
+
+		for (String str : myCustomAnnotationList) {
+			System.out.println(str);
+		}
+
+		writeToFile(myCustomAnnotationList, new File("myCustomAnnotation.txt"));
+
 	}
-	
-	public static void getMyCustomAnnotation(Class<?> customClass) {
-		
-		Field[] fields = customClass.getDeclaredFields();
-		
-		for(int i = 0; i < fields.length; i++) {
-			Field field = fields[i];
-			
-			if(field.getAnnotation(MyCustomAnnotation.class) instanceof MyCustomAnnotation) {
-				System.out.println(field.getType()+ " " +field.getName()+ " => ");
-				System.out.println("annotation type = "+ field.getAnnotation(MyCustomAnnotation.class).annotationType());
-				System.out.println("annotation value = "+ field.getAnnotation(MyCustomAnnotation.class).value());
+
+	public static List<String> getAnnotation(Class<?> customClass) {
+
+		List<String> annotationValue = new ArrayList<String>();
+
+		java.lang.reflect.Field[] fields = customClass.getDeclaredFields();
+
+		for (int i = 0; i < fields.length; i++) {
+			java.lang.reflect.Field field = fields[i];
+
+			if (((AnnotatedElement) field).getAnnotation(MyCustomAnnotation.class) instanceof MyCustomAnnotation) {
+				annotationValue.add(((java.lang.reflect.Field) field).getType() + " " + field.getName() + " -> "
+						+ ((AnnotatedElement) field).getAnnotation(MyCustomAnnotation.class).value());
 			}
 		}
+
+		return annotationValue;
+
+	}
+
+	public static void writeToFile(List<String> list, File file) throws Exception {
+		OutputStream os = new FileOutputStream(file);
+		ObjectOutputStream oos = new ObjectOutputStream(os);
+		oos.writeObject(list);
+		oos.close();
+		os.close();
 	}
 
 }
